39dc1c10411c798c49d4b1fd01bf806a7f8293fb
n7-remax <ditritusa@gmail.com>
2020-12-10 17:15:24 +0200

    added pagination

--- src/components/News/News.js
+++ src/components/News/News.js
@@ -8,6 +8,7 @@ import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 import CardHeader from '@material-ui/core/CardHeader';
 import CardMedia from '@material-ui/core/CardMedia';
+import Pagination from '@material-ui/lab/Pagination';
 import moment from 'moment'
 import axios from 'axios';
 
@@ -42,8 +43,10 @@ class News extends Component {
         articles: [],
         totalResults: null,
         category: 'business',
-        country: 'us'
-
+        country: 'us',
+        currentPage: 1,
+        startIndex: 0,
+        endIndex: 10,
     }
 
     getArticles = () => {
@@ -55,7 +58,6 @@ class News extends Component {
                     totalResults: response.data.totalResults
                 })
             })
-
     }
 
     componentDidMount() {
@@ -64,11 +66,27 @@ class News extends Component {
 
     }
     componentDidUpdate(nextProps, nextState) {
+        // get new respond on category change
         if (this.state.category !== nextState.category) {
             console.log('update')
             this.getArticles()
         }
-
+        // update component on current page increment 
+        if(this.state.currentPage > nextState.currentPage) {
+            console.log("next")
+            this.setState({
+                startIndex: (this.state.currentPage - 1) * 10,
+                endIndex: this.state.currentPage * 10
+            })
+        }
+        // update component on current page decrement 
+        if(this.state.currentPage < nextState.currentPage) {
+            console.log("back")
+            this.setState({
+                startIndex: (this.state.currentPage - 1) * 10 ,
+                endIndex: this.state.currentPage * 10 
+            })
+        }
     }
 
     selectCategoryHandler = (event) => {
@@ -77,10 +95,16 @@ class News extends Component {
             category: event.target.value
         })
     }
+    handlePageChange = (event,value) =>{
+        this.setState({
+            currentPage: value
+        })
+    }
 
     render() {
+        const totalPages = Math.ceil(this.state.totalResults / 10)
         const { classes } = this.props;
-        const newsList = this.state.articles.map((news, i) => {
+        const newsList = this.state.articles.slice(this.state.startIndex, this.state.endIndex).map((news, i) => {
             return (
                 <Card className={classes.root} key={i}>
                     <a className="card-link" href={news.url} target="_blank" rel="noopener noreferrer">
@@ -114,7 +138,9 @@ class News extends Component {
                 <div className="cards-wrapper">
                     {newsList}
                 </div>
-
+                <div className="pagination">
+                <Pagination count={totalPages} page={this.state.currentPage} onChange={this.handlePageChange} showFirstButton showLastButton size="large" />
+                </div>
             </div>
         );
     }
