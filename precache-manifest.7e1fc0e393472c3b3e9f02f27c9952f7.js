05078f6708f53f16fa95691dd8de1570af7c81f0
n7-remax <ditritusa@gmail.com>
2020-12-10 18:05:06 +0200

    Updates

---
+++ precache-manifest.7e1fc0e393472c3b3e9f02f27c9952f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2454da2c3365b3d36586c189d934aaa1",
+    "url": "/react-news/index.html"
+  },
+  {
+    "revision": "3b54da14b254e7fcbb48",
+    "url": "/react-news/static/css/main.b413abeb.chunk.css"
+  },
+  {
+    "revision": "4d0467ce5a3345be544f",
+    "url": "/react-news/static/js/2.1b19f861.chunk.js"
+  },
+  {
+    "revision": "556d72da1c19c1e88e2917fa6505d7f0",
+    "url": "/react-news/static/js/2.1b19f861.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b54da14b254e7fcbb48",
+    "url": "/react-news/static/js/main.c5c44d44.chunk.js"
+  },
+  {
+    "revision": "98d257c57d1eaa55b94d",
+    "url": "/react-news/static/js/runtime-main.715d2bb7.js"
+  }
+]);
